a9f797ea056fec75dc3fc1f1d6c92f4cf617295f
Faruk Hossain <miloniitju@gmail.com>
2025-09-25 22:19:22 +0800

    Add all helm charts, demo server-side service discovery

--- section_17/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ section_17/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -3,6 +3,7 @@ package com.eazybytes.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -15,6 +16,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
@@ -30,7 +32,7 @@ public class GatewayserverApplication {
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
 										.setFallbackUri("forward:/contactSupport")))
-						.uri("lb://ACCOUNTS")
+						.uri("http://accounts:8080")
 				)
 				.route(p -> p
 						.path("/eazybank/loans/**")
@@ -39,7 +41,7 @@ public class GatewayserverApplication {
 								.retry(retryConfig -> retryConfig.setRetries(3)
 										.setMethods(HttpMethod.GET)
 										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
-						.uri("lb://LOANS")
+						.uri("http://loans:8090")
 				)
 				.route(p -> p
 						.path("/eazybank/cards/**")
@@ -47,7 +49,7 @@ public class GatewayserverApplication {
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
 								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
 								.setKeyResolver(userKeyResolver())))
-						.uri("lb://CARDS")
+						.uri("http://cards:9000")
 				).build();
 	}
 
